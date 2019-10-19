f2d1db1cfdbc3ea6556d424dddb2c4335396b7f7
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-19 15:13:18 +0300

    add title to index view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.views.generic import RedirectView
+
 
 # Create your views here.
 
 def index(request):
+    
+    title = 'AudRate'
+    
+    context = {
+        'title': title,
+    }
             
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
